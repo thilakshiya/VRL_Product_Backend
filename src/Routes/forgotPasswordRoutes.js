bd43d98f0e8027f61216c8064bcffdc021a40666
thilakshiya <thilakshiyathilakshi@gmail.com>
2025-12-12 15:00:43 +0530

    btwo

--- src/Routes/forgotPasswordRoutes.js
+++ src/Routes/forgotPasswordRoutes.js
@@ -4,16 +4,6 @@ import { requestPasswordReset, resetPassword } from "../Controllers/forgotPasswo
 
 const router = express.Router();
 
-// Step 1: Request reset link
-// router.post("/request", requestPasswordReset);
-
-// Step 2: Reset password
-// router.post("/reset/:token", resetPassword);
-// import express from "express";
-// import { sendResetOTP, resetPassword } from "../controllers/passwordController.js";
-
-// const router = express.Router();
-
 router.post("/send-otp", requestPasswordReset);
 router.post("/reset", resetPassword);
 
@@ -23,20 +13,6 @@ router.post("/reset", resetPassword);
 export default router;
 
 
-// import express from "express";
-// import {
-//   requestPasswordReset,
-//   resetPassword,
-// } from "../Controllers/forgotPasswordController.js";
-
-// const router = express.Router();
-
-// // Send OTP
-// router.post("/send-otp", requestPasswordReset);
-
-// // Verify OTP + Reset Password
-// router.post("/reset", resetPassword);
-
-// export default router;  
+ 
 
 
